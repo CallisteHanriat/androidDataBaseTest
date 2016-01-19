00f4cce2edfb3588358d31081f2ca2585a9ebd55
CallisteHanriat <couim38@gmail.com>
2016-01-19 22:52:08 +0100

    AJout des noms dans une liste pris en compte

--- DataBaseTest/app/src/main/java/com/example/couim/databasetest/MainActivity.java
+++ DataBaseTest/app/src/main/java/com/example/couim/databasetest/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,11 +13,13 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listPerson;
     private Adapter adapter;
     private ArrayList<Person> allPersons;
+    private ArrayList<Person> allPersonTest;
     private PersonDAO personDAO;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,23 +34,35 @@ public class MainActivity extends AppCompatActivity {
         this.personDAO.open();
         this.allPersons = this.personDAO.findAllPersons();
 
-
-
         if(this.allPersons == null) {
             Toast.makeText(getApplicationContext(), "dataBase empty", Toast.LENGTH_SHORT).show();
-        } else {
-            this.adapter = new Adapter(getBaseContext(), this.allPersons);
-            this.listPerson = (ListView) findViewById(R.id.list_person);
-            this.listPerson.setAdapter(this.adapter);
         }
 
+        this.allPersons = new ArrayList<Person>();
+        this.adapter = new Adapter(getBaseContext(), this.allPersons);
+        this.listPerson = (ListView) findViewById(R.id.list_person);
+        this.listPerson.setAdapter(this.adapter);
 
+        allPersonTest = new ArrayList<Person>();
+        allPersonTest.add(new Person("Sebastien", "Patrick"));
+        allPersonTest.add(new Person("Lagneux", "Nicolas"));
+        allPersonTest.add(new Person("LightFoot", "David"));
+        allPersonTest.add(new Person("Culet", "Annie"));
+        allPersonTest.add(new Person("Saugnier", "Valentin"));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                //        .setAction("Action", null).show();
-
+                Random nb = new Random();
+                int n = 4 - 0 + 1;
+                int i = nb.nextInt()%n;
+                int randomNum = 0+i;
+                if (randomNum < 0)
+                    randomNum = -randomNum;
+                System.out.println("randomNum value : " + randomNum);
+                Log.d("randomNum value : ", Integer.toString(randomNum));
+                MainActivity.this.allPersons.add(MainActivity.this.allPersonTest.get(randomNum));
+                Toast.makeText(getApplicationContext(), MainActivity.this.allPersonTest.get(randomNum).getName() + " selected", Toast.LENGTH_SHORT).show();
+                MainActivity.this.listPerson.setAdapter(MainActivity.this.adapter);
             }
         });
     }
